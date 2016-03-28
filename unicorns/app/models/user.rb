384186d5ff06b74de756c481d27263f08af5e0fb
timlkelly <timlkelly@gmail.com>
2016-03-28 15:15:17 -0500

    schema and secure_password

--- unicorns/app/models/user.rb
+++ unicorns/app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   has_many :owned_games
   has_many :reviews
   has_many :games, through: :owned_games
-  has_many :reviewed_games, through: :reviews, source: :games 
+  has_many :reviewed_games, through: :reviews, source: :games
+
+  has_secure_password
 end
