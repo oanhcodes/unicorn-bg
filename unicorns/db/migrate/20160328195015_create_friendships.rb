b8a9430d7e5c0ac4bf767070e54e7cdd337f659f
timlkelly <timlkelly@gmail.com>
2016-03-28 15:28:52 -0500

    correct friendships migration

--- unicorns/db/migrate/20160328195015_create_friendships.rb
+++ unicorns/db/migrate/20160328195015_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.string :friend_requestor
-      t.string :friend_acceptor
+      t.references :friend_requestor_id
+      t.references :friend_acceptor_id
 
       t.timestamps null: false
     end
