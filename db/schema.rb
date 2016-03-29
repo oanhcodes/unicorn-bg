450f416fd500d0bd8f20ccf111f50b9b8575ddcf
oanhcodes <linda.oanh.ho@gmail.com>
2016-03-29 15:39:19 -0500

    Update game migrations and initialize AJAX api call

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328195045) do
+ActiveRecord::Schema.define(version: 20160329185153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,12 +38,14 @@ ActiveRecord::Schema.define(version: 20160328195045) do
   end
 
   create_table "games", force: :cascade do |t|
-    t.string   "title"
-    t.string   "image_url"
     t.string   "description"
-    t.string   "number_of_players"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "gameId"
+    t.string   "name"
+    t.string   "image"
+    t.integer  "minPlayers"
+    t.integer  "maxPlayers"
   end
 
   create_table "owned_games", force: :cascade do |t|
