f0476f853780bf20f20c7d278e617d45d939b110
timlkelly <timlkelly@gmail.com>
2016-03-30 13:43:09 -0500

    fix some syntax errors that were breaking the asset pipeline in heroku

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,43 +1,42 @@
-
 $(document).ready(function() {
 
 
-$(".review-container").on("submit", "form", function(event) {
-  event.preventDefault();
-  var $form = $(this);
-  var formUrl = $form.attr("action");
-  var formMethod = $form.attr("method");
-  var data = $form.serialize();
-  var request = $.ajax({
-    type: formMethod,
-    url: formUrl,
-    data: data
-    });
-  request.done(function(response){
-    $(".review-list").prepend(response);
-    $('form').each(function(){
-      this.reset();
+  $(".review-container").on("submit", "form", function(event) {
+    event.preventDefault();
+    var $form = $(this);
+    var formUrl = $form.attr("action");
+    var formMethod = $form.attr("method");
+    var data = $form.serialize();
+    var request = $.ajax({
+      type: formMethod,
+      url: formUrl,
+      data: data
       });
-    });
+    request.done(function(response){
+      $(".review-list").prepend(response);
+      $('form').each(function(){
+        this.reset();
+        });
+      });
 
-  $(".search-box").on("submit", ".game_search", function(event){
+    $(".search-box").on("submit", ".game_search", function(event){
       event.preventDefault();
 
       var query = $(this).find('input[type="search"]').val();
       var request = $.get('/search/' + query, function(response){
         $(".game-list").append(response);
-      })
+      });
 
 
       request.fail(function(xhr, status, errorThrown) {
         if(xhr.status === 500) {
-          window.location = "/500"
+          window.location = "/500";
         }
-      })
+      });
+    });
   });
+
 });
-
-
 
 // $(document).ready(function() {
 // Number of times = average rating
