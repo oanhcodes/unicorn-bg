fe486d3b994ad872db6976a6345ff035f0f3ad14
oanhcodes <linda.oanh.ho@gmail.com>
2016-03-30 14:03:56 -0500

    update overall styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      UserMailer.signup_confirmation(user).deliver_now
       session[:user_id] = user.id
       redirect_to '/'
     else
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
 
   # Specify what domain to user for mailer urls
@@ -42,6 +42,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.delivery_method = :smtp
-
 end
