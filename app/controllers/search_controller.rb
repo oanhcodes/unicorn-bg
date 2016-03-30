d64527f0b5df7386602648e27f8c10e1bb320703
LHAWLEY <hawleyleeanne@gmail.com>
2016-03-30 11:45:54 -0500

    Remove comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,15 +4,11 @@ require 'pry'
 class SearchController < ApplicationController
   def show
     xml = HTTP.get("http://www.boardgamegeek.com/xmlapi/search?search=#{params[:id]}").to_s
-    # Rails.logger.info("XML from http://www.boardgamegeek.com/xmlapi/search?search=#{params[:id]}")
-    # Rails.logger.info(xml)
     search_results = XmlSimple.xml_in(xml, { 'KeyAttr' => 'name' })
-    # binding.pry
+
     if search_results != nil
       id = search_results['boardgame'].first['objectid']
       json = HTTP.get("https://bgg-json.azurewebsites.net/thing/#{id}").to_s
-      # Rails.logger.info("JSON from https://bgg-json.azurewebsites.net/thing/#{id}")
-      # Rails.logger.info(json)
       boardgame = JSON.parse(json)
 
       game = Game.new({gameId: boardgame['gameId'],
